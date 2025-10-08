4a8953a56945102655ea4335a55c92a37c91ea47
Timothée Poux <timothee.poux@gmail.com>
2025-10-08 09:48:59 +0200

    ajout de toutes les touches

--- src/main/java/com/jad/Main.java
+++ src/main/java/com/jad/Main.java
@@ -12,14 +12,28 @@ public enum Main {
     public static void main(String[] args) {
         TextWindowSettings settings = new TextWindowSettings();
         settings.addKeyboardListener(KeyEvent.VK_ESCAPE, "exit");
+        /// touches du joueur 1
+        settings.addKeyboardListener(KeyEvent.VK_Q, "player1_left");
+        settings.addKeyboardListener(KeyEvent.VK_D, "player1_right");
+        settings.addKeyboardListener(KeyEvent.VK_Z, "player1_up");
+        settings.addKeyboardListener(KeyEvent.VK_S, "player1_down");
+        /// touches du joueur 2
+        settings.addKeyboardListener(KeyEvent.VK_NUMPAD4, "player2_left");
+        settings.addKeyboardListener(KeyEvent.VK_NUMPAD6, "player2_right");
+        settings.addKeyboardListener(KeyEvent.VK_NUMPAD8, "player2_up");
+        settings.addKeyboardListener(KeyEvent.VK_NUMPAD2, "player2_down");
+
+        settings.setBackgroundColor(Color.PINK);
+        settings.setScreenHeight(10);
+        settings.setScreenWidth(50);
+        settings.setFontSize(42);
         TextWindow textWindow = new TextWindow(settings);
         textWindow.setVisible(true);
 
         Point lastMousePosition;
         while (textWindow.isOff("exit")) {
-            lastMousePosition = textWindow.getMousePosition();
-            StringBuilder message = new StringBuilder("Press ESC to exit.\n");
-            textWindow.display(message.toString());
+            if (textWindow.isOn("player2_up")) textWindow.display("YOULOU");
+            if (textWindow.isOff("player2_up")) textWindow.display("Yagoulou");
         }
         textWindow.close();
     }
