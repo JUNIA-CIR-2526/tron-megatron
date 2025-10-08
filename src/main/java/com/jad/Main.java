893932d4907022b158d783b908b8cdd39b23a359
Timothée Poux <timothee.poux@gmail.com>
2025-10-08 11:01:50 +0200

    ajout de toutes les touches

--- src/main/java/com/jad/Main.java
+++ src/main/java/com/jad/Main.java
@@ -15,13 +15,9 @@ public enum Main {
         /* touches du joueur 1 */
         settings.addKeyboardListener(KeyEvent.VK_Q, "player1_left");
         settings.addKeyboardListener(KeyEvent.VK_D, "player1_right");
-        settings.addKeyboardListener(KeyEvent.VK_Z, "player1_up");
-        settings.addKeyboardListener(KeyEvent.VK_S, "player1_down");
         /* touches du joueur 2 */
         settings.addKeyboardListener(KeyEvent.VK_NUMPAD4, "player2_left");
         settings.addKeyboardListener(KeyEvent.VK_NUMPAD6, "player2_right");
-        settings.addKeyboardListener(KeyEvent.VK_NUMPAD8, "player2_up");
-        settings.addKeyboardListener(KeyEvent.VK_NUMPAD2, "player2_down");
 
         settings.setBackgroundColor(Color.PINK);
         settings.setScreenHeight(10);
