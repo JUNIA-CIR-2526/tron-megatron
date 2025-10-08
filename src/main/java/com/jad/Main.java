629cb4d2e208fb79c62a9f028f2d983885696722
Timothée Poux <timothee.poux@gmail.com>
2025-10-08 09:52:11 +0200

    ajout de toutes les touches

--- src/main/java/com/jad/Main.java
+++ src/main/java/com/jad/Main.java
@@ -12,12 +12,12 @@ public enum Main {
     public static void main(String[] args) {
         TextWindowSettings settings = new TextWindowSettings();
         settings.addKeyboardListener(KeyEvent.VK_ESCAPE, "exit");
-        /// touches du joueur 1
+        /* touches du joueur 1 */
         settings.addKeyboardListener(KeyEvent.VK_Q, "player1_left");
         settings.addKeyboardListener(KeyEvent.VK_D, "player1_right");
         settings.addKeyboardListener(KeyEvent.VK_Z, "player1_up");
         settings.addKeyboardListener(KeyEvent.VK_S, "player1_down");
-        /// touches du joueur 2
+        /* touches du joueur 2 */
         settings.addKeyboardListener(KeyEvent.VK_NUMPAD4, "player2_left");
         settings.addKeyboardListener(KeyEvent.VK_NUMPAD6, "player2_right");
         settings.addKeyboardListener(KeyEvent.VK_NUMPAD8, "player2_up");
